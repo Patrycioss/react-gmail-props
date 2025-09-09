541bd759c03e880bef92d094eca96ebbd19ba5b3
Patrick Schuur <patrickschuurdev@gmail.com>
2025-09-09 16:06:17 +0200

    Setup and email.jsx + emails.jsx

---
+++ src/Emails.jsx
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+import Email from "./Email.jsx";
+
+function Emails(props) {
+    return (
+        <main className="emails">
+            <ul>
+                {props.filteredEmails.map((email, index) => (
+                    <Email email={email} key={index} toggleRead={props.toggleRead} toggleStar={props.toggleStar} />
+                ))}
+            </ul>
+        </main>
+    );
+}
+
+export default Emails;
